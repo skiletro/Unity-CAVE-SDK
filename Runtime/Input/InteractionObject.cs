fd13210c5c603fe03c95ff6a41438579af242b6a
Casper Helliwell <68733738+cnmhqwerty@users.noreply.github.com>
2025-09-04 15:14:05 +0100

    Update InteractionObject.cs

--- Runtime/Input/InteractionObject.cs
+++ Runtime/Input/InteractionObject.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 namespace MMUCAVE
 {
+    /// <summary>
+    /// All Interaction Objects respond to being tapped through implementation of OnTouch.
+    /// <u>Requires a collider on the object</u>
+    /// </summary>
     public abstract class InteractionObject : MonoBehaviour
     {
         /// <summary>
-        /// All Interaction Objects must implement OnTouch as its primary function
-        /// Requires a collider on the object to call OnTouch
+        /// All Interaction Objects must implement OnTouch as the reaction to any tap events.
         /// </summary>
+        /// <example>
+        /// <code>
+        /// public override void OnTouch()
+        /// {
+        ///    CAVE.transform.position = transform.position;
+        ///    CAVE.transform.rotation = Quaternion.Euler(rotationOffset);
+        /// }
+        /// </code>
+        /// </example>
         public abstract void OnTouch();
     }
 }
