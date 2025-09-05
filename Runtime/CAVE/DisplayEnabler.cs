41fb6d3ab2aaca736a10a64d72f61311f0775266
Casper Helliwell <68733738+cnmhqwerty@users.noreply.github.com>
2025-09-05 09:28:32 +0100

    Resolved build error with pre-processor checks

--- Runtime/CAVE/DisplayEnabler.cs
+++ Runtime/CAVE/DisplayEnabler.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class DisplayEnabler : MonoBehaviour
 {
-    #if UNITY_STANDALONE && !UNITY_EDITOR
+    
     // When running in a build, manually enable all screens for accurate projection
         [Tooltip("Number of displays connected to the system (-1 for indexing)")][SerializeField] private int displayIndex = 0;
         
         private void OnEnable()
         {
+        #if !UNITY_EDITOR
             if (displayIndex >= Display.displays.Length)
             {
                 Debug.LogError("Display index out of range");
@@ -15,6 +16,7 @@ public class DisplayEnabler : MonoBehaviour
             }
             
             Display.displays[displayIndex].Activate();// Activate all possible displays
+        #endif
         }
-    #endif
+    
 }
